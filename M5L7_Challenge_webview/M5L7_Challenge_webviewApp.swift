cd27fea15688d58ffe77c6f6033c7c412b95241a
Tom Montgomery <montgomery3681@gmail.com>
2023-01-16 21:53:04 -0700

    Initial Commit

---
+++ M5L7_Challenge_webview/M5L7_Challenge_webviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  M5L7_Challenge_webviewApp.swift
+//  M5L7_Challenge_webview
+//
+//  Created by tom montgomery on 1/16/23.
+//
+
+import SwiftUI
+
+@main
+struct M5L7_Challenge_webviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
