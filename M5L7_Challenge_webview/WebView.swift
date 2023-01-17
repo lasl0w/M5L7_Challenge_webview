d4d7a6278a02571f6ebeb99554f9b6a8d0d3077b
Tom Montgomery <montgomery3681@gmail.com>
2023-01-16 22:15:32 -0700

    - render webView - implement basic UIViewRepresentable

---
+++ M5L7_Challenge_webview/WebView.swift
@@ -0,0 +1,38 @@
+//
+//  WebView.swift
+//  M5L7_Challenge_webview
+//
+//  Created by tom montgomery on 1/16/23.
+//
+
+import SwiftUI
+import WebKit
+
+struct WebView: UIViewRepresentable {
+
+    // implement a makeUIView
+    func makeUIView(context: Context) -> WKWebView {
+        // Instantiate the basic thing
+        let webView = WKWebView()
+        
+        let myURL = URL(string: "https://www.hackingwithswift.com/")
+        // Create the request and load it
+        if myURL != nil {
+            let request = URLRequest(url: myURL!)
+            webView.load(request)
+        }
+        return webView
+       
+    }
+    
+    // implement a updateUIView
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        
+    }
+}
+
+struct WebView_Previews: PreviewProvider {
+    static var previews: some View {
+        WebView()
+    }
+}
